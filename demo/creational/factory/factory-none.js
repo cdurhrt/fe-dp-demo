4690bde6c3521db522677398093180a7434273e7
cdurheart <dumingliang1995@163.com>
2022-05-06 18:13:39 +0800

    factory

---
+++ demo/creational/factory/factory-none.js
@@ -0,0 +1,9 @@
+class Bmw {
+  constructor(model, price, maxSpeed) {
+    this.model = model;
+    this.price = price;
+    this.maxSpeed = maxSpeed;
+  }
+}
+
+const bmwX5 = new Bmw('X5', 108000, 300)
